44c1a49c440234f2b116584f11ad2526fdf11111
Igor Kheidorov <igor.kheidorov@gmail.com>
2017-02-02 19:10:27 +0300

    Intitial project version

---
+++ src/igorhmm/currenttime/CurrentTime.java
@@ -0,0 +1,19 @@
+package igorhmm.currenttime;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+	 
+/**
+ * Class to determine current system time 
+ */	 
+public class CurrentTime{
+	/**
+	 * Returns current system time in given format
+	 * @param  timeFormat Desirable format for current time output
+	 * @return current time
+	 */	 
+	static public String getTime(String timeFormat) {
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat(timeFormat);
+		return sdf.format(cal.getTime());
+	}
+}
---
+++ src/igorhmm/currenttime/EntryPoint.java
@@ -0,0 +1,11 @@
+package igorhmm.currenttime;
+
+/**
+ * Entry point to software to get current time,
+ * No input parameters from CL are used
+ */
+public class EntryPoint{
+	public static void main(String[] args){		
+		System.out.println("Current time is " + CurrentTime.getTime("HH:mm:ss"));
+	}
+}
