1f718145be86da92c2c770667ac3aaeb5a96b395
Eugene Vanifatev <eugenevanifatev@gmail.com>
2017-02-05 16:11:31 +0300

    fix comment

--- src/igorhmm/currenttime/EntryPoint.java
+++ src/igorhmm/currenttime/EntryPoint.java
@@ -5,7 +5,8 @@ package igorhmm.currenttime;
  * No input parameters from CL are used
  */
 public class EntryPoint {
- public static void main(String[] args) {		
-   System.out.println("Current time is " + CurrentTime.getTime("HH:mm")); // TODO: make time format as constant value
+ public static void main(String[] args) { 
+   // TODO: make time format as constant value	
+   System.out.println("Current time is " + CurrentTime.getTime("HH:mm")); 
   }
 }
